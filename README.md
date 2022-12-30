8b5c46667d75462fd09660a1fdb421419fca7cea
Daniel Warren <dpwarren455@gmail.com>
2022-12-30 14:37:09 -0800

    Tried to fix verify

---
+++ README.md
@@ -0,0 +1,4 @@
+I'm not sure why but the verification fails sometimes when the files aren't corrupt.
+
+Just run the script again or use the command `sha256sum -c sha256sums.txt`
+in the directory you stored the data
--- download.py
+++ download.py
@@ -16,29 +16,32 @@ def ensure_path(path):
 
 def download_sha256sums(url):
     sha256sums = {}
+    sha256sums_txt = ""
     
     comments_url = f'{url}/comments/sha256sum.txt'
     submissions_url = f'{url}/submissions/sha256sums.txt'
     
     for response, response_url in [(requests.get(comments_url), comments_url), (requests.get(submissions_url), submissions_url)]:
         if response.status_code == 200:
+            sha256sums_txt += response.text
             for line in response.text.splitlines():
                 hash, filename = line.split()
                 sha256sums[filename] = hash
         else:
             logger.error(f'Error downloading {response_url}: {response.status_code}')
     
+    with open(files_dir / "sha256sums.txt", "w") as f:
+        f.write(sha256sums_txt)
+
     return sha256sums
-    
+
 
 def verify(filename):
     if filename in sha256sums:
-        with open(files / filename, 'rb+') as f:
+        with open(files_dir / filename, 'rb') as f:
             checksum = hashlib.sha256()
-            file_buffer = f.read(65536)
-            while len(file_buffer) > 0:
-                checksum.update(file_buffer)
-                file_buffer = f.read(65536)
+            while chunk := f.read(65536):
+                checksum.update(chunk)
             if checksum.hexdigest() == sha256sums[filename]:
                 return True
             else:
@@ -47,7 +50,7 @@ def verify(filename):
     return True
 
 
-def download_file(filename, retries=0):
+def download_file(filename):
     if "RC" in filename:
         download_url = f'{url}/comments/{filename}'
     elif "RS" in filename:
@@ -56,7 +59,7 @@ def download_file(filename, retries=0):
         logger.error(f'Invalid filename: {filename}')
         return False
 
-    path = files.joinpath(filename)
+    path = files_dir / filename
 
     if not path.exists():
         try:
@@ -64,27 +67,21 @@ def download_file(filename, retries=0):
             if response.status_code == 200:
                 with open(path, 'wb') as f:
                     logger.info(f'Downloading {filename}')
-                    for data in response.iter_content(chunk_size=65536):
+                    for data in response.iter_content(chunk_size=1048576):
                         f.write(data)
                     logger.info(f'Downloaded {filename}')
                     check = verify(filename)
                     if check:
                         logger.info(f'{filename} verified')
-
                         return True
+
                     else:
+                        # I'm not sure why but this fails sometimes when the files aren't corrupt
+                        # Just run the script again or use the command `sha256sum -c sha256sums.txt`
+                        # in the directory you stored the data
                         logger.error(
-                            f'{filename} corrupted, retrying download')
-
-                        if retries < 1:
-                            path.unlink()  # delete corrupted file
-                            # retry download
-                            return download_file(filename, retries=retries + 1)
-                        else:
-                            logger.error(
-                                f'{filename} download failed after 1 retry')
-
-                            return False
+                            f'{filename} may be corrupted')
+                        return False
             else:
                 logger.error(
                     f'Error downloading {filename}: {response.status_code}')
@@ -101,16 +98,13 @@ def download_file(filename, retries=0):
             return True
         else:
             logger.error(f'{filename} corrupted, retrying download')
-
             path.unlink()  # delete corrupted file
             if retries < 1:
                 # retry download
                 return download_file(filename, retries=retries + 1)
             else:
                 logger.error(f'{filename} download failed after 1 retry')
-
                 return False
-
 
 parser = argparse.ArgumentParser(
     description="This is a program that extracts data from Reddit's public comment and submission history and stores it in a specified directory.")
@@ -127,45 +121,39 @@ parser.add_argument("-t", "--threads",
 args = parser.parse_args()
 
 log_dir = "./logs"
-
 ensure_path(log_dir)
 
 logging.basicConfig(filename=f'{log_dir}/download.log',
                     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s [%(threadName)s] ",
-                    level=logging.INFO)
+                    level=logging.INFO,
+                    filemode="w")
 logger = logging.getLogger()
 
 # create a console handler
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
-
-# create a formatter and set it on the console handler
 formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    '%(asctime)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(formatter)
-
-# add the console handler to the logger
 logger.addHandler(console_handler)
-
-# set the logger level
 logger.setLevel(logging.INFO)
 
 if args.verbose:
     logger.setLevel(logging.DEBUG)
 
-files = Path(args.directory)
+files_dir = Path(args.directory)
 url = "https://files.pushshift.io/reddit"
 reddit_comments = []
 reddit_submissions = []
 
 dr = pd.date_range(args.start_date, args.end_date,
-                   freq='MS').strftime('%Y-%m').tolist()
+                freq='MS').strftime('%Y-%m').tolist()
 
 for date in dr:
     reddit_comments.append(str(f'RC_{date}.zst'))
     reddit_submissions.append(str(f'RS_{date}.zst'))
 
-ensure_path(files)
+ensure_path(files_dir)
 sha256sums = download_sha256sums(url)
 if sha256sums is None:
     logger.error('Failed to download sha256sums')
